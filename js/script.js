4f5621dd491d4f85b08ca4aacb064b5df7824f2b
EthanLi126 <ethan.li@ucc.on.ca>
2020-04-24 21:34:58 -0400

    first push

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+window.onload = function() {
+  document.getElementById("l-back").style.display = "none";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("navbar").style.display = "initial";
+  document.getElementById("maindiv").style.display = "initial";
+}
+
+
