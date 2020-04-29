c5fe008f4a44cedd9cb810d7de1d24ac9b29ef52
EthanLi126 <ethan.li@ucc.on.ca>
2020-04-28 21:07:45 -0400

    animated menu button

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@
 window.onload = function() {
   document.getElementById("l-back").style.display = "none";
   document.getElementById("loader").style.display = "none";
-  document.getElementById("navbar").style.display = "initial";
   document.getElementById("maindiv").style.display = "initial";
 }
+
+
+
+function menu(x) {
+  if (x.classList.contains("change")) {
+    x.classList.remove("change"); 
+    x.classList.add("changerev");
+
+
+  } else {
+    x.classList.remove("changerev");
+    x.classList.add("change");
+
+  }
+
+}
+
 
 
