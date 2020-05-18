b2f4544a7488f9b0add885d4563de602b954ecd8
EthanLi126 <ethan.li@ucc.on.ca>
2020-05-18 19:43:36 -0400

    added animated search bar, about/contact page, courreges brand

--- js/script.js
+++ js/script.js
@@ -34,6 +34,8 @@ function menu(x) {
     x.classList.add("changerev");
     document.getElementById("menu").style.height = "0%";
     document.getElementById("menu2").style.height = "0%";
+    document.getElementById("scontainer").style.width = "0%";
+    document.getElementById("scontainer").style.opacity = "0";
 
   } else {
     x.classList.remove("changerev");
@@ -65,7 +67,40 @@ function move() {
 
 
 
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    document.getElementById("myUL").style.display = "block";
 
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+
+        if (input.value.length == 0) {
+            document.getElementById("myUL").style.display = "none";
+        } else if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function openSearch() {
+  var wi = document.getElementById('scontainer').style.width;
+
+  if (wi !== "60%") {
+    document.getElementById("scontainer").style.width = "60%";
+    document.getElementById("scontainer").style.opacity = "1";
+
+  } else {
+    document.getElementById("scontainer").style.width = "0%";
+    document.getElementById("scontainer").style.opacity = "0";
+  }
+}
 
 
 
--- js/script1.js
+++ js/script1.js
@@ -53,3 +53,39 @@ function openTab(tabName) {
   document.getElementById(tabName).style.width = "50%";
   document.getElementById(tabName).style.opacity = "1";
 }
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    document.getElementById("myUL").style.display = "block";
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+
+        if (input.value.length == 0) {
+            document.getElementById("myUL").style.display = "none";
+        } else if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+function openSearch() {
+  var wi = document.getElementById('scontainer').style.width;
+
+  if (wi !== "60%") {
+    document.getElementById("scontainer").style.width = "60%";
+    document.getElementById("scontainer").style.opacity = "1";
+
+  } else {
+    document.getElementById("scontainer").style.width = "0%";
+    document.getElementById("scontainer").style.opacity = "0";
+  }
+}
